ade3e7f6fb58ecad8dbea85ef7116916ff61fecd
tiwariParth <243mjparth@gmail.com>
2024-11-08 17:08:05 +0530

    feat:Script can monitor top performance stats

--- ServerStat.sh
+++ ServerStat.sh
@@ -1,3 +1,20 @@
-#!/bin/bash 
+#!/bin/bash
 
-echo "Hello WORLD"
+echo "Server Performance Stats"
+echo "------------------------"
+echo "Total CPU Usage:"
+mpstat | grep -A 5 "%idle" | tail -n 1 | awk '{print 100 - $ 12"%"}'
+
+echo "Total Memory Usage:"
+free -m | awk 'NR==2{printf "Used: %sMB (%.2f%%), Free: %sMB (%.2f%%)\n", $3, $3*100/$2, $4, $4*100/$2}'
+
+echo "Total Disk Usage:"
+df -h --total | grep 'total' | awk '{printf "Used: %s (%s), Free: %s\n", $3, $5, $4}'
+
+echo "Top 5 Processes by CPU Usage:"
+ps -eo pid,ppid,cmd,%mem,%cpu --sort=-%cpu | head -n 6
+
+echo "Top 5 Processes by Memory Usage:"
+ps -eo pid,ppid,cmd,%mem,%cpu --sort=-%mem | head -n 6
+
+echo "------------------------"
